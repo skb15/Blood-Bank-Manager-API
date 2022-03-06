e42af678a140fff8eb95990731426f1f418719f7
Sourav Kumar Baidya <souravbaidya32@gmail.com>
2022-03-06 12:49:31 +0530

    CORS Enable Data Migrated To Backend

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from hospitals import database
 
 app = Flask(__name__)
+CORS(app)
 
 
 def parse_blood(bloodgroup):
--- hospitals.py
+++ hospitals.py
@@ -1,6 +1,7 @@
 database = [
     {
         "id": 1,
+        "tags": ["govt", "verified"],
         "name": "Seth Sukhlal Karnani Memorial Hospital",
         "address": "SSKM Hospital Rd, Bhowanipore, Kolkata, West Bengal",
         "pincode": 700020,
@@ -8,15 +9,17 @@ database = [
                 "lat": 22.5389,
                 "long": 88.3409
         },
-        "stock": {
+        "stocks": {
             "A": {"+": 11, "-": 8},
             "B": {"+": 10, "-": 0},
             "AB": {"+": 12, "-": 15},
             "O": {"+": 5, "-": 0}
-        }
+        },
+        "lastUpdate": "2022-03-05T09:06:00.000Z",
     },
     {
         "id": 2,
+        "tags": ["govt", "verified"],
         "name": "Ramakrishna Mission Seva Pratishthan(Sishu Mangal)",
         "address": "Sarat Bose Road, Hazra, Kalighat, Kolkata, West Bengal",
         "pincode": 700029,
@@ -24,15 +27,17 @@ database = [
                 "lat": 22.5233,
                 "long": 88.3521
         },
-        "stock": {
+        "stocks": {
             "A": {"+": 17, "-": 5},
             "B": {"+": 13, "-": 0},
             "AB": {"+": 5, "-": 0},
             "O": {"+": 3, "-": 1}
-        }
+        },
+        "lastUpdate": '2022-03-06T03:13:00.000Z',
     },
     {
         "id": 3,
+        "tags": ["govt", "verified"],
         "name": "Baruipur S D Hospital",
         "address": "Kulpi Road, Baruipur, Kolkata",
         "pincode": 700144,
@@ -40,11 +45,26 @@ database = [
                 "lat": 22.3655,
                 "long": 88.4325
         },
-        "stock": {
+        "stocks": {
             "A": {"+": 7, "-": 12},
             "B": {"+": 0, "-": 0},
             "AB": {"+": 17, "-": 9},
             "O": {"+": 5, "-": 0}
-        }
-    }
+        },
+        "lastUpdate": '2022-03-04T09:10:00.000Z',
+    },
+    {
+        "id": 4,
+        "tags": ["govt", "verified"],
+        "name": "Subhashgram General Hospital",
+        "address": "Subhasgram, Kolkata, West Bengal",
+        "pincode": 700147,
+        "stocks": {
+            "A": {"+": 25, "-": 12},
+            "B": {"+": 3, "-": 1},
+            "AB": {"+": 13, "-": 2},
+            "O": {"+": 28, "-": 0},
+        },
+        "lastUpdate": "2022-03-04T09:06:00.000Z",
+    },
 ]
